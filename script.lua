de28179562e3740e11b8925bf1ed430e12de1453
user26567457872 <aaronfortullel@gmail.com>
2026-03-08 21:05:40 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,15 +1,9 @@
--- Advanced Auto Parry
-
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local VIM = game:GetService("VirtualInputManager")
 
 local player = Players.LocalPlayer
 local hrp
-
-local BASE_DISTANCE = 22
-local SPEED_MULT = 0.55
-local PREDICT = 0.2
 
 local function updateChar()
     local char = player.Character or player.CharacterAdded:Wait()
@@ -21,57 +15,46 @@ player.CharacterAdded:Connect(updateChar)
 
 local function getBall()
 
-for _,v in pairs(workspace:GetChildren()) do
-    if v:IsA("BasePart") and v.Name:lower():find("ball") then
-        return v
-    end
-end
+    local fastest
+    local speed = 0
 
+    for _,v in pairs(workspace:GetChildren()) do
+        if v:IsA("BasePart") then
+
+            local vel = v.AssemblyLinearVelocity
+            local mag = vel.Magnitude
+
+            if mag > speed and mag > 20 then
+                speed = mag
+                fastest = v
+            end
+
+        end
+    end
+
+    return fastest
 end
 
 local function parry()
-
-VIM:SendMouseButtonEvent(0,0,0,true,game,0)
-task.wait()
-VIM:SendMouseButtonEvent(0,0,0,false,game,0)
-
+    VIM:SendMouseButtonEvent(0,0,0,true,game,0)
+    task.wait()
+    VIM:SendMouseButtonEvent(0,0,0,false,game,0)
 end
 
 RunService.Heartbeat:Connect(function()
 
-if not hrp then return end
+    if not hrp then return end
 
-local ball = getBall()
-if not ball then return end
+    local ball = getBall()
+    if not ball then return end
 
-local vel = ball.AssemblyLinearVelocity
-local speed = vel.Magnitude
+    local vel = ball.AssemblyLinearVelocity
+    local speed = vel.Magnitude
 
-if speed < 5 then return end
+    local distance = (ball.Position - hrp.Position).Magnitude
 
-local predicted = ball.Position + vel * PREDICT
-
-local distance = (predicted - hrp.Position).Magnitude
-
-local dynamic = BASE_DISTANCE + (speed * SPEED_MULT)
-
-local direction = (hrp.Position - ball.Position).Unit
-local dot = vel.Unit:Dot(direction)
-
-if speed > 120 then
-dynamic = dynamic + 10
-end
-
-if dot > 0.2 and distance <= dynamic then
-
-parry()
-
-if speed > 150 then
-parry()
-parry()
-end
-
-end
+    if speed > 20 and distance < 25 then
+        parry()
+    end
 
 end)
-print("AUTO PARRY SCRIPT INICIADO"))
